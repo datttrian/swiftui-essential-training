1c383f5705444b498d6864c716c026eb0eaf08c9
T. Tran <ttran375@my.centennialcollege.ca>
2023-12-27 10:59:32 -0800

    Iteration with ForEach

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var orders:[Int] = [1]
+    var orders:[Int] = [1,2,3,4,6]
     var body: some View {
         VStack {
             ZStack {
@@ -20,23 +20,41 @@ struct ContentView: View {
                 Text("Order Pizza").font(.title)
                 Spacer()
             }
-            HStack(alignment:.firstTextBaseline) {
-                Text("Your Order item")
-                Spacer()
-                Text(19.90, format: .currency(code: "USD"))   
+            ScrollView
+            {
+                ForEach(orders, id:\.self) { order in
+                    HStack(alignment:.firstTextBaseline) {
+                        Text("Your Order item \(order)")
+                        Spacer()
+                        Text(19.90, format: .currency(code: "USD"))
+                    }}
             }
+            
+            
             VStack {
                 Image(systemName: "rectangle.fill")
                 Text("Margherita")
                 Text("Description")
             }
-            HStack(alignment:.top, spacing: 15) {
-                Image(systemName: "1.circle.fill").font(.largeTitle)
-                VStack(alignment:.leading) {
-                    Text("Margherita")
-                    Text("Description")
+            
+            
+            ScrollView 
+            {
+                ForEach(1...35, id:\.self)
+                {
+                    item in HStack(alignment:.top, spacing: 15)
+                    {
+                        Image(systemName: "\(item).circle.fill").font(.largeTitle)
+                        VStack(alignment:.leading) 
+                        {
+                            Text("Margherita")
+                            Text("Description")
+                        }
+                    }
                 }
             }
+            
+            
             Spacer()
         }
         .padding()
