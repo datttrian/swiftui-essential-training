8b8d04713795d852d94dfd4e4df2946c53d2b6a5
T. Tran <ttran375@my.centennialcollege.ca>
2023-12-27 10:08:19 -0800

    Start a new SwiftUI project for iOS

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Huli Pizza Company")
+            Text("Order Pizza")
+            
         }
         .padding()
     }
