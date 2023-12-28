32ca43292e5a1210f72f3d7195dd3780d6586696
T. Tran <ttran375@my.centennialcollege.ca>
2023-12-28 08:17:47 -0800

    Images and assets

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -11,50 +11,57 @@ struct ContentView: View {
     var orders:[Int] = [1,2,3,4,6]
     var body: some View {
         VStack {
+            
+            
+            
             ZStack {
-                Image("surfBanner").resizable().scaledToFit()
-                Text("Huli Pizza Company").background()
+                Image("surfBanner")
+                    .resizable()
+                .scaledToFit()
+                Text("Huli Pizza Company")
+                    .background()
             }
-            Image(systemName: orders.isEmpty ? "cart": "cart.circle.fill")
+            Image(systemName: orders.isEmpty ? "cart" : "cart.circle.fill")
             HStack {
-                Text("Order Pizza").font(.title)
+                
+                Text("Order Pizza")
+                    .font(.title)
                 Spacer()
             }
-            ScrollView
-            {
-                ForEach(orders, id:\.self) { order in
-                    HStack(alignment:.firstTextBaseline) {
+            ScrollView{
+                ForEach(orders,id:\.self){ order in
+                    HStack(alignment:.firstTextBaseline){
                         Text("Your Order item \(order)")
                         Spacer()
                         Text(19.90, format: .currency(code: "USD"))
-                    }}
+                    }
+                }
+                
             }
-            
-            
             VStack {
-                Image(systemName: "rectangle.fill")
+                if let image = UIImage(named: "0_lg"){
+                    Image(uiImage: image)
+                } else {
+                    Image("surfboard_lg")
+                }
                 Text("Margherita")
                 Text("Description")
             }
-            
-            
-            ScrollView 
-            {
-                ForEach(1...35, id:\.self)
-                {
-                    item in HStack(alignment:.top, spacing: 15)
-                    {
-                        Image(systemName: "\(item).circle.fill").font(.largeTitle)
-                        VStack(alignment:.leading) 
-                        {
+            ScrollView{
+                ForEach(1...25,id:\.self){ item in
+                    HStack(alignment:.top,spacing:15) {
+                        if let image = UIImage(named: "\(item)_sm"){
+                            Image(uiImage: image)
+                        } else {
+                            Image("surfboard_sm")
+                        }
+                        VStack(alignment:.leading) {
                             Text("Margherita")
                             Text("Description")
                         }
                     }
                 }
             }
-            
-            
             Spacer()
         }
         .padding()
