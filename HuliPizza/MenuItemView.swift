3170921f124e68fdd9c27ec06565551d093eefdb
T. Tran <ttran375@my.centennialcollege.ca>
2023-12-28 10:39:02 -0800

    Text modifiers

--- HuliPizza/MenuItemView.swift
+++ HuliPizza/MenuItemView.swift
@@ -17,8 +17,18 @@ struct MenuItemView: View {
             } else {
                 Image("surfboard_lg")
             }
-            Text("Margherita")
-            Text("Description")
+            VStack(alignment: .leading) {
+                Text("Margherita")
+                    .font(.title)
+//                    .bold()
+                    .fontWeight(.semibold)
+                
+                ScrollView {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum porta est, non maximus libero varius in. Nullam quis risus sed lectus elementum elementum. Nullam sit amet mi vel odio sollicitudin commodo. Mauris fermentum nibh magna, ut blandit velit malesuada quis. Mauris scelerisque dictum mi nec molestie. Vivamus in semper.")
+//                        .font(.body)
+                        .font(.custom("Georgia", size: 18, relativeTo: .body))
+                }
+            }
         }
     }
 }
