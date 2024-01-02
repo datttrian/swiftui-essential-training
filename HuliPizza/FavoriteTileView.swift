94df418f60ade6c2844722996e392121861f6c60
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-02 04:56:19 -0800

    Solution: Favorites grid

---
+++ HuliPizza/FavoriteTileView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteTileView.swift
+//  HuliPizza
+//
+//  Created by datttrian on 2024-01-02.
+//
+
+import SwiftUI
+
+struct FavoriteTileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteTileView()
+}
