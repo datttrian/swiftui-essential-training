0cd78ddc887da758688360e514d87ddfe1a7d50b
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-02 09:33:55 -0800

    SwiftUI to storyboards

--- HuliPizzaPay/HuliPizzaPay/ViewController.swift
+++ HuliPizzaPay/HuliPizzaPay/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 import SwiftUI
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
     @IBSegueAction func HuliPizzaMenu3(_ coder: NSCoder) -> UIViewController? {
         return UIHostingController(coder: coder, rootView: MenuView3(menu: MenuModel().menu) )
